d475c135a9ffef4a4a6bc23ecd043559d2244060
Jonas Moreira <jonas.zuazo@ufabc.edu.br>
2024-04-13 21:06:01 -0300

    Setup view model lib and create RegisterViewModel.

--- app/src/main/java/com/ufabc/docchain/presentation/RegisterActivity.kt
+++ app/src/main/java/com/ufabc/docchain/presentation/RegisterActivity.kt
@@ -1,12 +1,15 @@
 package com.ufabc.docchain.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ufabc.docchain.R
 import com.ufabc.docchain.databinding.RegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val viewModel: RegisterViewModel by viewModels()
 
     private lateinit var binding: RegisterBinding
 
---
+++ app/src/main/java/com/ufabc/docchain/presentation/RegisterViewModel.kt
@@ -0,0 +1,8 @@
+package com.ufabc.docchain.presentation
+
+import androidx.lifecycle.ViewModel
+import com.ufabc.docchain.data.FirebaseAuthRepository
+
+class RegisterViewModel(authRepository: FirebaseAuthRepository) : ViewModel() {
+
+}
