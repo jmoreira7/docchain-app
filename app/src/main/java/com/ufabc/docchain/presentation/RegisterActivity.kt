b127291724a7d4875dee6e4db8ccd3383cdaf145
Jonas Moreira <jonas.zuazo@ufabc.edu.br>
2024-04-13 22:45:14 -0300

    Create RegisterViewModel and integrate createUser use case with UI.

--- app/src/main/java/com/ufabc/docchain/presentation/RegisterActivity.kt
+++ app/src/main/java/com/ufabc/docchain/presentation/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.ufabc.docchain.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -19,11 +20,49 @@ class RegisterActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.register)
 
         setupViews()
+        setupViewModel()
     }
 
     private fun setupViews() {
         binding.registerBackButton.setOnClickListener {
             finish()
         }
+
+        binding.registerRegisterButton.setOnClickListener {
+            val email = binding.registerEmailTextInputEditText.text.toString()
+            val password = binding.registerPasswordTextInputEditText.text.toString()
+
+            viewModel.createUser(email, password)
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.action.observe(this) { action ->
+            when (action) {
+                is RegisterViewModel.RegisterViewModelAction.showCreateUserSuccessDialog -> {
+                    Toast.makeText(
+                        this,
+                        "Usuário criado com sucesso!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    finish()
+                }
+
+                is RegisterViewModel.RegisterViewModelAction.showCreateUserFailDialog -> {
+                    Toast.makeText(
+                        this,
+                        "Criação de usuário falhou.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    finish()
+                }
+            }
+        }
+    }
+
+    interface RegisterInterface {
+        fun createUser(email: String, password: String)
     }
 }
--- app/src/main/java/com/ufabc/docchain/presentation/RegisterViewModel.kt
+++ app/src/main/java/com/ufabc/docchain/presentation/RegisterViewModel.kt
@@ -1,8 +1,44 @@
 package com.ufabc.docchain.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ufabc.docchain.data.FirebaseAuthRepository
+import com.ufabc.docchain.presentation.RegisterViewModel.RegisterViewModelAction.showCreateUserFailDialog
+import com.ufabc.docchain.presentation.RegisterViewModel.RegisterViewModelAction.showCreateUserSuccessDialog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class RegisterViewModel(authRepository: FirebaseAuthRepository) : ViewModel() {
+class RegisterViewModel() : ViewModel(), RegisterActivity.RegisterInterface {
+
+    private val authRepository: FirebaseAuthRepository = FirebaseAuthRepository()
+
+    private val _action = MutableLiveData<RegisterViewModelAction>()
+
+    val action: LiveData<RegisterViewModelAction>
+        get() = _action
+
+    override fun createUser(email: String, password: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val success = authRepository.createUser(email, password)
+
+            if (success) {
+                postAction(showCreateUserSuccessDialog)
+            } else {
+                postAction(showCreateUserFailDialog)
+            }
+        }
+    }
+
+    private fun postAction(action: RegisterViewModelAction) {
+        _action.value = action
+    }
+
+    sealed class RegisterViewModelAction() {
+        object showCreateUserSuccessDialog : RegisterViewModelAction()
+
+        object showCreateUserFailDialog : RegisterViewModelAction()
+    }
 
 }
